dbef611d24b119990da79c642fb84b83b932bff4
appeon <tao.wu@appeon.com>
2017-07-13 09:00:14 +0800

    add msgbox

--- testsrccontrol/testsrccontrolDlg.cpp
+++ testsrccontrol/testsrccontrolDlg.cpp
@@ -99,6 +99,7 @@ BOOL CtestsrccontrolDlg::OnInitDialog()
 	SetIcon(m_hIcon, FALSE);		// Set small icon
 
 	// TODO: Add extra initialization here
+	MessageBox(L"tset)";
 
 	return TRUE;  // return TRUE  unless you set the focus to a control
 }
